5bf9c1227abd6bf7b3e16eb30dfc50ea0e4e4b5a
phongvo <vophongqt1998@gmail.com>
2022-11-30 15:10:18 +0700

    Initial Commit

---
+++ Firebase App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase App
+//
+//  Created by Vo Duc Phong on 30/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
